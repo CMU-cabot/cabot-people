4ce6085dc45da35ef60ddaf6fdd729cc632b71ec
Daisuke Sato <daisukes@cmu.edu>
2023-12-08 10:47:10 -0500

    add launch-test.sh script to test make sure the program launches without ERROR
    Signed-off-by: Daisuke Sato <daisukes@cmu.edu>

---
+++ test/launch-test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+trap ctrl_c INT QUIT TERM
+
+function ctrl_c() {
+    docker compose down > /dev/null 2>&1
+    exit
+}
+function red {
+    echo -en "\033[31m"  ## red
+    echo $@
+    echo -en "\033[0m"  ## reset color
+}
+
+pwd=`pwd`
+scriptdir=`dirname $0`
+cd $scriptdir
+scriptdir=`pwd`
+cd $scriptdir/../
+
+# reset realsense
+docker compose -f docker-compose-test-rs3.yaml run rs1 sudo /resetrs.sh
+
+# launch detection and tracking, and check if it output any ERROR logs
+(timeout 20s docker compose -f docker-compose-test-rs3.yaml up rs1 track 2>&1) | while IFS= read -r line
+do
+    echo "$line"  # Optional: Echoes the output of the command
+    if [[ "$line" == *"ERROR"* ]]; then
+        echo "Error detected, aborting the command."
+        pkill -P $$  # Kills the process group, effectively stopping the command
+	exit 1
+    fi
+done
