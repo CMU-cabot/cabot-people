058270886e0144e3588a67bf192e4bf0e2c4068a
Allan <allanwangliqian@gmail.com>
2024-07-11 18:47:48 +0900

    lidar speed estimate implementation initiated

--- lidar_process/launch/scan_receiver.launch.py
+++ lidar_process/launch/scan_receiver.launch.py
@@ -23,6 +23,12 @@ def generate_launch_description():
     namespace = LaunchConfiguration('namespace')
     ring_limit = LaunchConfiguration('ring_limit')
     scan_max_range = LaunchConfiguration('scan_max_range')
+    history_window = LaunchConfiguration('history_window')
+    future_window = LaunchConfiguration('future_window')
+    low_level_pos_threshold = LaunchConfiguration('low_level_pos_threshold')
+    high_level_pos_threshold = LaunchConfiguration('high_level_pos_threshold')
+    high_level_vel_threshold = LaunchConfiguration('high_level_vel_threshold')
+    high_level_ori_threshold = LaunchConfiguration('high_level_ori_threshold')
 
     return LaunchDescription([
         # save all log file in the directory where the launch.log file is saved
@@ -33,15 +39,30 @@ def generate_launch_description():
         DeclareLaunchArgument('namespace', default_value='lidar'),
         DeclareLaunchArgument('ring_limit', default_value='7'),
         DeclareLaunchArgument('scan_max_range', default_value='100'),
+        DeclareLaunchArgument('history_window', default_value='8'),
+        DeclareLaunchArgument('future_window', default_value='8'),
+        DeclareLaunchArgument('low_level_pos_threshold', default_value='0.25'),
+        DeclareLaunchArgument('high_level_pos_threshold', default_value='1'),
+        DeclareLaunchArgument('high_level_vel_threshold', default_value='1'),
+        DeclareLaunchArgument('high_level_ori_threshold', default_value='1'),
 
         # overwrite parameters
         SetParameter(name='ring_limit', value=ring_limit),
         SetParameter(name='scan_max_range', value=scan_max_range),
+        SetParameter(name='history_window', value=history_window),
+        SetParameter(name='future_window', value=future_window),
+        SetParameter('low_level_pos_threshold', value=low_level_pos_threshold),
+        SetParameter('high_level_pos_threshold', value=high_level_pos_threshold),
+        SetParameter('high_level_vel_threshold', value=high_level_vel_threshold),
+        SetParameter('high_level_ori_threshold', value=high_level_ori_threshold),
 
         Node(
             package="lidar_process",
             executable="scan_receiver",
             name="scan_receiver",
             namespace=namespace,
+            output="both",
+            emulate_tty=True,
+            #arguments=["--ros-args", "--log-level", "debug"]
         ),
     ])
