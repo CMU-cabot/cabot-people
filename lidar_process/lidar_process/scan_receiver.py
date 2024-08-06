b46ba257e289259637d79d34a9101efe14c16c65
Allan <allanwangliqian@gmail.com>
2024-08-06 11:09:41 +0900

    pointcloud velocity estimate finished (not debugged)

--- lidar_process/lidar_process/scan_receiver.py
+++ lidar_process/lidar_process/scan_receiver.py
@@ -1,6 +1,7 @@
 import signal
 import sys
 import queue
+import time
 import threading
 import numpy as np
 from sklearn.cluster import DBSCAN
@@ -13,9 +14,11 @@ from std_msgs.msg import Header
 from sensor_msgs.msg import PointCloud2, PointField
 from sensor_msgs_py import point_cloud2
 from cabot_msgs.msg import PoseLog
+from . import pcl_to_numpy
 
 from rclpy.executors import MultiThreadedExecutor
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
+from rclpy.qos import QoSProfile, QoSDurabilityPolicy
 
 from cabot_msgs.srv import LookupTransform
 
@@ -28,10 +31,14 @@ class ScanReceiver(Node):
 
         state_update_callback_group = MutuallyExclusiveCallbackGroup()
         transform_lookup_callback_group = MutuallyExclusiveCallbackGroup()
+        visualization_callback_group = MutuallyExclusiveCallbackGroup()
+        transient_local_qos = QoSProfile(depth=1, durability=QoSDurabilityPolicy.TRANSIENT_LOCAL)
+
         self.scan_sub = self.create_subscription(
             PointCloud2, 
             '/velodyne_points_cropped',
             self.scan_cb,
+            #qos_profile=transient_local_qos,
             10,
             callback_group = state_update_callback_group
         )
@@ -39,6 +46,7 @@ class ScanReceiver(Node):
             PoseLog, 
             '/cabot/pose_log',
             self.pose_cb,
+            #qos_profile=transient_local_qos,
             10,
             callback_group = state_update_callback_group
         )
@@ -46,9 +54,18 @@ class ScanReceiver(Node):
         self.pcl_debug_pub = self.create_publisher(
             PointCloud2,
             "/map_velodyne_points",
+            #qos_profile=transient_local_qos,
             10,
-            callback_group = transform_lookup_callback_group
+            callback_group = visualization_callback_group
         )
+
+        self.lookup_transform_service = self.create_client(
+            LookupTransform, 
+            '/lookup_transform', 
+            #qos_profile=transient_local_qos,
+            callback_group=transform_lookup_callback_group
+        )
+
 
         self.pointcloud = np.array([])
         self.pointcloud_prev = np.array([])
@@ -73,8 +90,6 @@ class ScanReceiver(Node):
         self._high_level_pos_threshold = self.declare_parameter('high_level_pos_threshold', 1).value
         self._high_level_vel_threshold = self.declare_parameter('high_level_vel_threshold', 1).value
         self._high_level_ori_threshold = self.declare_parameter('high_level_ori_threshold', 1).value
-
-        self.lookup_transform_service = self.create_client(LookupTransform, '/lookup_transform')
 
         self.pointcloud_history = queue.Queue(self._history_window)
         self.pointcloud_history_np = []  # list of numpys
@@ -127,6 +142,9 @@ class ScanReceiver(Node):
     def scan_cb(self, msg):
         # Stores pointcloud when the node receives one.
 
+        self.get_logger().info("pcl received")
+        start_time = time.time()
+
         if self.pose is None:
             return
 
@@ -134,16 +152,46 @@ class ScanReceiver(Node):
         self.curr_time = msg.header.stamp.sec + msg.header.stamp.nanosec * 1e-9
         dt = self.curr_time - self.prev_time
 
-        self.get_logger().info("pcl received")
-
         # Pointcloud format
         # (x, y, z, intensity, ring number [e.g. 0-15])
         # Pointcloud complete format
         # (x, y, z, intensity, ring, group id, group center x, group center y, vel_x, vel_y)
-        parsed_pointcloud = point_cloud2.read_points(msg, skip_nans=True)
+        parsed_pointcloud_1 = pcl_to_numpy.read_points_numpy(
+            msg, 
+            field_names=['x', 'y', 'z', 'intensity'], 
+            skip_nans=True)
+        parsed_pointcloud_2 = pcl_to_numpy.read_points_numpy(
+            msg, 
+            field_names=['ring'], 
+            skip_nans=True)
 
-        num_raw_pt = len(parsed_pointcloud)
+        tmp_start_time = time.time()
+        num_raw_pt = len(parsed_pointcloud_1)
         if num_raw_pt > 0:
+            num_field_1 = len(parsed_pointcloud_1[0])
+            num_field_2 = len(parsed_pointcloud_2[0])
+            self.pointcloud = np.zeros((num_raw_pt, num_field_1 + num_field_2))
+            self.pointcloud[:, :num_field_1] = parsed_pointcloud_1
+            self.pointcloud[:, num_field_1:] = parsed_pointcloud_2
+            #for i, elem in enumerate(parsed_pointcloud):
+            #    self.pointcloud[i, :] = list(elem)
+            self.pointcloud = self._filter_pointcloud(self.pointcloud)
+
+            # old, much slower ways to parse point clouds
+            """
+            for i, elem in enumerate(parsed_pointcloud):
+                self.pointcloud[i, :] = list(elem)
+            self.pointcloud = self._filter_pointcloud(self.pointcloud)
+            """
+            """
+            self.get_logger().info(f"type(parsed_pointcloud)={type(parsed_pointcloud[0])}")
+            self.pointcloud = parsed_pointcloud
+            points_array = np.array(parsed_pointcloud)
+            print("points_array=", points_array)
+            print("self.pointcloud=", self.pointcloud)
+            self.pointcloud = self._filter_pointcloud(self.pointcloud)
+            """
+            """
             self.pointcloud = np.zeros((num_raw_pt, len(parsed_pointcloud[0])))
             validation_array = np.zeros(num_raw_pt)
             for i, elem in enumerate(parsed_pointcloud):
@@ -151,26 +199,31 @@ class ScanReceiver(Node):
                 if self._is_valid_pt(elem):
                     validation_array[i] = 1
             self.pointcloud = self.pointcloud[validation_array == 1]
+            """
         else:
             self.pointcloud = np.array([])
+        print(time.time() - tmp_start_time)
 
         # transform pointcloud to map frame
-        self.get_logger().info("lidar transform start")
         lidar_to_map = self.lookup_transform('velodyne', 'map')
-        self.get_logger().info("transform received")
         transformed_cloud = self._do_transform(self.pointcloud, lidar_to_map, msg.header)
-        self.get_logger().info("lidar transform end")
 
         print("--------pcl debug--------")
-        print(self.pointcloud[0])
-        print(transformed_cloud[0])
+        print(len(self.pointcloud))
+        if len(self.pointcloud) > 0:
+            print(self.pointcloud[0])
+            print(transformed_cloud[0])
 
-        self.pointcloud_complete = self._calc_pointcloud_group_vel(
-                                            self.pointcloud_prev, 
-                                            transformed_cloud, 
-                                            self.prev_pose, 
-                                            self.curr_pose,
-                                            dt)
+            print("stuck here 1")
+            self.pointcloud_complete = self._calc_pointcloud_group_vel(
+                                                self.pointcloud_prev, 
+                                                transformed_cloud, 
+                                                self.prev_pose, 
+                                                self.curr_pose,
+                                                dt)
+            print("not stuck here 1")
+        else:
+            self.pointcloud_complete = []
         self.pointcloud_prev = self.pointcloud_complete 
         self.prev_pose = self.curr_pose
         self.prev_time = self.curr_time
@@ -179,13 +232,26 @@ class ScanReceiver(Node):
             self.is_history_complete = True
             # pop the oldest point cloud from the queue
             tmp = self.pointcloud_history.get(block=False)
+        print("stuck here 2")
         self.pointcloud_history.put(self.pointcloud, block=False)
+        print("not stuck here 2")
+
+        print("This callback runs: {} seconds".format(time.time() - start_time))
         
         return
     
     def pose_cb(self, msg):
         self.pose = msg.pose
         return
+    
+    def _filter_pointcloud(self, pointcloud):
+        if not (self._ring_limit == -1):
+            condition = (pointcloud[:, 4] == self._ring_limit)
+            pointcloud = pointcloud[condition == True]
+        if self._scan_max_range < 100:
+            condition = (np.linalg.norm(pointcloud[:, :2], axis=1) < self._scan_max_range)
+            pointcloud = pointcloud[condition == True]
+        return pointcloud
     
     def _is_valid_pt(self, elem):
         # test if ring specified
@@ -219,21 +285,18 @@ class ScanReceiver(Node):
         t[:3, :3] = Rotation.from_quat([q.x, q.y, q.z, q.w]).as_matrix()
         t[:3, 3] = [x.x, x.y, x.z]
 
-        print(len(pointcloud_coord))
+        print("Number of point clouds: {}".format(len(pointcloud_coord)))
 
         pointcloud_new = np.ones((len(pointcloud_coord), 4))
         pointcloud_new[:, :3] = pointcloud_coord
         pointcloud_new = np.matmul(t, pointcloud_new.T)
-        print("DEBUG0")
         pointcloud_new[0, :] = np.divide(pointcloud_new[0, :], pointcloud_new[3, :])
         pointcloud_new[1, :] = np.divide(pointcloud_new[1, :], pointcloud_new[3, :])
         pointcloud_new[2, :] = np.divide(pointcloud_new[2, :], pointcloud_new[3, :])
         pointcloud_new = pointcloud_new.T
 
-        pointcloud_rst = pointcloud
+        pointcloud_rst = np.copy(pointcloud)
         pointcloud_rst[:, :3] = pointcloud_new[:, :3]
-
-        print("DEBUG1")
 
         # for debug/visualization purpose
         if not header is None:
@@ -247,8 +310,6 @@ class ScanReceiver(Node):
             pointcloud_map = point_cloud2.create_cloud(pointcloud_map_header, fields, pointcloud_new[:, :3])
             self.pcl_debug_pub.publish(pointcloud_map)
 
-        print("DEBUG2")
-
         return pointcloud_rst
     
     def _calc_pointcloud_group_vel(self, prev_ptcloud, curr_ptcloud, prev_pose, curr_pose, dt):
@@ -257,6 +318,9 @@ class ScanReceiver(Node):
         #
         # returns pointcloud with low level group (pedestrian/object) information
         # and estimated velocities
+
+        assert(dt > 0)
+
         print("----------Pose & time info----------")
         print(prev_pose)
         print(curr_pose)
@@ -268,6 +332,7 @@ class ScanReceiver(Node):
         complete_ptcloud[:, :5] = curr_ptcloud
         ptcloud_pos = curr_ptcloud[:, :3]
         # z coordinates are not used for now, so use _
+        print("stuck here 3")
         (group, 
          group_x, 
          group_y, 
@@ -276,6 +341,7 @@ class ScanReceiver(Node):
          unique_group_x, 
          unique_group_y, 
          _) = self._get_groups_and_centers(ptcloud_pos)
+        print("not stuck here 3")
         complete_ptcloud[:, 5] = group
         complete_ptcloud[:, 6] = group_x
         complete_ptcloud[:, 7] = group_y
@@ -299,9 +365,43 @@ class ScanReceiver(Node):
                 unique_group_x_prev[i] = group_x_prev[group_prev == l][0]
                 unique_group_y_prev[i] = group_y_prev[group_prev == l][0]
 
-            # TODO
             # get matching pairs from curr unique groups and prev unique groups
             # for each pair: estimate a velocity and propagate the velocity to point clouds
+            for i, gp_index in enumerate(unique_group):
+                gp_mask = (group == gp_index)
+                gp_x = group_x[gp_mask]
+                gp_y = group_y[gp_mask]
+                range_x = np.max(gp_x) - np.min(gp_x)
+                range_y = np.max(gp_y) - np.min(gp_y)
+                # A bit ad hoc, but if the obsacle is too big
+                # It is likely a static obstacle and should have 0 velocity
+                large_obs_threshold = 5
+                if (range_x > large_obs_threshold) or (range_y > large_obs_threshold):
+                    continue
+
+                # Assume the group that was closest before was the prior group
+                # If too far, then it is likely a new group
+                gp_center_x = unique_group_x[i]
+                gp_center_y = unique_group_y[i]
+                new_group_dist_threshold = 1
+                min_dist = np.inf
+                min_prev_x = None
+                min_prev_y = None
+                for j in range(len(unique_group_prev)):
+                    gp_prev_center_x = unique_group_x_prev[j]
+                    gp_prev_center_y = unique_group_y_prev[j]
+                    dist = np.sqrt((gp_center_x - gp_prev_center_x) ** 2 + 
+                                   (gp_center_y - gp_prev_center_y) ** 2)
+                    if dist < min_dist:
+                        min_dist = dist
+                        min_prev_x = gp_prev_center_x
+                        min_prev_y = gp_prev_center_y
+
+                if min_dist < new_group_dist_threshold:
+                    group_vel_x = (gp_center_x - min_prev_x) / dt
+                    group_vel_y = (gp_center_y - min_prev_y) / dt
+                    complete_ptcloud[gp_mask, 8] = group_vel_x
+                    complete_ptcloud[gp_mask, 9] = group_vel_y
 
         return complete_ptcloud
     
@@ -319,11 +419,14 @@ class ScanReceiver(Node):
         group_x = np.zeros(num_pts)
         group_y = np.zeros(num_pts)
         group_z = np.zeros(num_pts)
+        print("stuck here 4")
+        print(num_pts)
         group = self._get_groups(
                     ptcloud, 
                     self._low_level_pos_threshold, 
                     self._low_level_core_samples
                     )
+        print("not stuck here 4")
         labels = np.unique(group)
         num_labels = len(labels)
         group_x_unique = np.zeros(num_labels)
@@ -344,16 +447,17 @@ class ScanReceiver(Node):
             group_y[group == l] = group_y_val
             group_z[group == l] = group_z_val
 
-        # [:1] means excluding noise group (first element is always -1)
-        assert(labels[0] == -1)
+        if labels[0] == -1:
+            labels = labels[1:]
+
         return (group, 
-                group_x, 
-                group_y, 
-                group_z, 
-                labels[1:], 
-                group_x_unique[1:], 
-                group_y_unique[1:], 
-                group_z_unique[1:])
+            group_x, 
+            group_y, 
+            group_z, 
+            labels, 
+            group_x_unique, 
+            group_y_unique, 
+            group_z_unique)
     
     def _get_groups(self, data, threshold, core_samples):
         # performs DBSCAN and clusters according to threshold and 
@@ -401,6 +505,8 @@ def main():
     except KeyboardInterrupt:
         #process_thread.join()
         scan_receiver.get_logger().info("Shutting down")
+    except Exception as err:
+        scan_receiver.get_logger().error(err)
 
     scan_receiver.destroy_node()
     rclpy.shutdown()
