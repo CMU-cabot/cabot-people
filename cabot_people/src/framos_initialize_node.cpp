52d719cd0b50a08c03083741951f35e5509dc4c1
Tatsuya Ishihara <tisihara@jp.ibm.com>
2025-03-13 14:32:40 +0900

    fix license, do not reset RealSense individually
    DCO 1.1 Signed-off-by: Tatsuya Ishihara <tisihara@jp.ibm.com>

--- cabot_people/src/framos_initialize_node.cpp
+++ cabot_people/src/framos_initialize_node.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2025  Carnegie Mellon University, IBM Corporation, and others
+// Copyright (c) 2025  IBM Corporation
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
